dbd90d9bfeedef9117a828af5ea62856efc4bd60
gotechdevweb <vuhai91az@gmail.com>
2026-01-06 23:16:06 +0700

    commit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -2,11 +2,31 @@ import MainLayout from "../../layouts/MainLayout";
 import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 import { Link } from "react-router-dom";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 const Login = () => {
   const [isVisibility, setIsVisibility] = useState(false);
   const handleTogglePassword = () => {
     setIsVisibility(!isVisibility);
   };
+
+  const loginSchema = z.object({
+    email: z.string().min(1, "Please enter email").email("Email is invalid"),
+    password: z.string().min(6, "Password must be at least 6 character"),
+  });
+  type LoginFormData = z.infer<typeof loginSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(loginSchema),
+  });
+
+  const onSubmit = async () => {};
   return (
     <MainLayout>
       <div className="rounded-[20px] w-full sm:w-[500px] mx-auto my-[30px] overflow-hidden">
@@ -14,18 +34,28 @@ const Login = () => {
           Login
         </h3>
 
-        <form className="p-[20px] flex flex-col gap-[20px] text-[16px]">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="p-[20px] flex flex-col gap-[20px] text-[16px]"
+        >
           <div className="flex flex-col gap-[10px]">
-            <label>Email / Mobile</label>
+            <label>Email </label>
             <input
-              placeholder="email/mobile"
+              {...register("email")}
+              placeholder="email"
               className="py-[10px] px-[20px] border-gray-400 border-[1px] rounded-[10px]"
             />
+            {errors.email && (
+              <div className="mt-2 rounded-md bg-red-50 px-3 py-2 text-sm text-red-600 border border-red-200">
+                {errors.email.message}
+              </div>
+            )}
           </div>
           <div className="flex flex-col gap-[10px]">
             <label>Password</label>
             <div className="flex">
               <input
+                {...register("password")}
                 type={isVisibility ? "text" : "password"}
                 placeholder="Password"
                 className=" py-[10px] flex-1 w-full px-[20px] border-gray-400 border-[1px] rounded-tl-[10px] rounded-bl-[10px]"
@@ -41,6 +71,11 @@ const Login = () => {
                 )}
               </div>
             </div>
+            {errors.password && (
+              <div className="mt-2 rounded-md bg-red-50 px-3 py-2 text-sm text-red-600 border border-red-200">
+                {errors.password.message}
+              </div>
+            )}
           </div>
           <p className="text-right text-[12px] ">
             Forgot your{" "}
--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -2,12 +2,33 @@ import MainLayout from "../../layouts/MainLayout";
 import { useState } from "react";
 import { Eye, EyeOff } from "lucide-react";
 import { Link } from "react-router-dom";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 
 const Register = () => {
   const [isVisibility, setIsVisibility] = useState(false);
   const handleTogglePassword = () => {
     setIsVisibility(!isVisibility);
   };
+
+  const registerSchema = z.object({
+    email: z.string().min(5, "Please enter email").email("Email is invalid"),
+    password: z.string().min(6, "Password must be at least 6 character"),
+    name: z.string().min(3, "Name must be at least 3 character"),
+  });
+
+  type RegisterFormData = z.infer<typeof registerSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(registerSchema),
+  });
+
+  const onSubmit = async () => {};
   return (
     <MainLayout>
       <div className="rounded-[20px] w-full sm:w-[500px] mx-auto my-[30px] overflow-hidden">
@@ -15,18 +36,41 @@ const Register = () => {
           Register
         </h3>
 
-        <form className="p-[20px] flex flex-col gap-[20px] text-[16px]">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="p-[20px] flex flex-col gap-[20px] text-[16px]"
+        >
           <div className="flex flex-col gap-[10px]">
-            <label>Email / Mobile</label>
+            <label>Name</label>
             <input
-              placeholder="email/mobile"
+              {...register("name")}
+              placeholder="name"
               className="py-[10px] px-[20px] border-gray-400 border-[1px] rounded-[10px]"
             />
+            {errors.name && (
+              <div className="mt-2 rounded-md bg-red-50 px-3 py-2 text-sm text-red-600 border border-red-200">
+                {errors.name.message}
+              </div>
+            )}
+          </div>
+          <div className="flex flex-col gap-[10px]">
+            <label>Email</label>
+            <input
+              {...register("email")}
+              placeholder="email"
+              className="py-[10px] px-[20px] border-gray-400 border-[1px] rounded-[10px]"
+            />
+            {errors.email && (
+              <div className="mt-2 rounded-md bg-red-50 px-3 py-2 text-sm text-red-600 border border-red-200">
+                {errors.email.message}
+              </div>
+            )}
           </div>
           <div className="flex flex-col gap-[10px]">
             <label>Password</label>
             <div className="flex">
               <input
+                {...register("password")}
                 type={isVisibility ? "text" : "password"}
                 placeholder="Password"
                 className=" py-[10px] flex-1 w-full px-[20px] border-gray-400 border-[1px] rounded-tl-[10px] rounded-bl-[10px]"
@@ -42,6 +86,11 @@ const Register = () => {
                 )}
               </div>
             </div>
+            {errors.password && (
+              <div className="mt-2 rounded-md bg-red-50 px-3 py-2 text-sm text-red-600 border border-red-200">
+                {errors.password.message}
+              </div>
+            )}
           </div>
           <p className="text-right text-[12px] ">
             Forgot your{" "}
@@ -54,7 +103,7 @@ const Register = () => {
           <p className="text-center text-[12px]">
             Did you have an account yet?{" "}
             <span className="text-[#14c9c9] font-semibold">
-              <Link to="/register"> Login Now</Link>
+              <Link to="/register">Login Now</Link>
             </span>
           </p>
         </form>
