506c38315c2378c80c40ffe267e98d2434b8cc8f
gotechdevweb <vuhai91az@gmail.com>
2026-03-01 23:47:58 +0700

    push

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
 import ProductDetail from "@pages/ProductDetail";
 import "./App.css";
-import HomePage from "./pages/HomePage";
 import { Routes, Route } from "react-router-dom";
-import ProductList from "@pages/ProductList";
 import Checkout from "@pages/Checkout";
-import AddressList from "./pages/AddressList";
-import PaymentMethods from "./pages/PaymentMethods";
-import Login from "@pages/Login";
-import Register from "@pages/Register";
-import BlogPage from "@pages/BlogPage";
-import BlogList from "@pages/BlogList";
-import AboutUs from "@pages/AboutUs";
+
 import ScrollHandler from "@components/ScrollHandler";
+import { Suspense, lazy } from "react";
+import LoadingSpinner from "@components/LoadingSpinner";
+
+const HomePage = lazy(() => import("@pages/HomePage"));
+const ProductList = lazy(() => import("@pages/ProductList"));
+const AddressList = lazy(() => import("@pages/AddressList"));
+const PaymentMethods = lazy(() => import("@pages/PaymentMethods"));
+const Login = lazy(() => import("@pages/Login"));
+const Register = lazy(() => import("@pages/Register"));
+const BlogPage = lazy(() => import("@pages/BlogPage"));
+const BlogList = lazy(() => import("@pages/BlogList"));
+const AboutUs = lazy(() => import("@pages/AboutUs"));
 
 function App() {
   return (
     <>
       <ScrollHandler />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/product/:id" element={<ProductDetail />} />
-        <Route path="/product-list" element={<ProductList />} />
-        <Route path="/blog/:id" element={<BlogPage />} />
-        <Route path="/blog-list" element={<BlogList />} />
-        <Route path="/cart" element={<Checkout />} />
-        <Route path="/checkout/address" element={<AddressList />} />
-        <Route path="/checkout/payment" element={<PaymentMethods />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/about-us" element={<AboutUs />} />
-      </Routes>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/product-list" element={<ProductList />} />
+          <Route path="/blog/:id" element={<BlogPage />} />
+          <Route path="/blog-list" element={<BlogList />} />
+          <Route path="/cart" element={<Checkout />} />
+          <Route path="/checkout/address" element={<AddressList />} />
+          <Route path="/checkout/payment" element={<PaymentMethods />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about-us" element={<AboutUs />} />
+        </Routes>
+      </Suspense>
     </>
   );
 }
